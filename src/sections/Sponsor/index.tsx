8255b36c988a4f7a5ee1f0b211fb28bed31cadc2
Stormer0528 <serhii1010@outlook.com>
2024-10-29 07:57:41 -0400

    Update spnosor tree

--- src/sections/Sponsor/index.tsx
+++ src/sections/Sponsor/index.tsx
@@ -1,13 +1,18 @@
-import { useMemo, useEffect } from 'react';
+import _ from 'lodash';
+import { useMemo, useState, useEffect, useCallback } from 'react';
 import {
   ReactFlow,
   type Node,
   type Edge,
+  useReactFlow,
   ReactFlowProvider,
   type FitViewOptions,
 } from '@xyflow/react';
 
 import Stack from '@mui/material/Stack';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
 
 import { paths } from 'src/routes/paths';
 
@@ -19,16 +24,18 @@ import {
   PLACEMENTTREE_NODE_Y_SPACE,
 } from 'src/consts';
 
+import { Iconify } from 'src/components/Iconify';
 import { Breadcrumbs } from 'src/components/Breadcrumbs';
 import ComponentBlock from 'src/components/Component-Block';
 import { LoadingScreen } from 'src/components/loading-screen';
-
-import { useFetchMembers } from 'src/sections/Profile/useApollo';
+import { usePopover, CustomPopover } from 'src/components/custom-popover';
 
 import { useAuthContext } from 'src/auth/hooks';
 
 import { StandardNode } from './node';
 import CustomEdge from './customEdge';
+import NodeContext from './nodeContext';
+import { useFetchMembers } from '../Profile/useApollo';
 
 const fitViewOptions: FitViewOptions = {
   padding: 0.2,
@@ -57,10 +64,10 @@ function buildSponsorTree(members: any[], me: any) {
     }
   });
 
-  return result;
+  return { result, memberMap };
 }
 
-function buildTree(node: any, baseX: number, depth: number, tree: any[]) {
+function buildTree(node: any, baseX: number, depth: number, tree: any[], visibleMap: any = null) {
   const { children } = node;
 
   if (children.length === 0) {
@@ -90,18 +97,27 @@ function buildTree(node: any, baseX: number, depth: number, tree: any[]) {
   let maxX = baseX;
   const positions: any[] = [];
 
-  children.forEach((child: any, idx: number) => {
-    const { maxX: tempX, position } = buildTree(
-      child,
-      maxX + (idx === 0 ? 0 : PLACEMENTTREE_NODE_X_SPACE),
-      depth + 1,
-      tree
-    );
-    maxX = tempX;
-    positions.push(position);
-  });
+  if (!visibleMap || visibleMap[node.id] === 2) {
+    children.forEach((child: any, idx: number) => {
+      const { maxX: tempX, position } = buildTree(
+        child,
+        maxX + (idx === 0 ? 0 : PLACEMENTTREE_NODE_X_SPACE),
+        depth + 1,
+        tree,
+        visibleMap
+      );
+      maxX = tempX;
+      positions.push(position);
+    });
+  }
 
-  const resPositionX = (maxX + baseX - PLACEMENTTREE_NODE_WIDTH) / 2;
+  let resPositionX = maxX;
+  if (!visibleMap || visibleMap[node.id] === 2) {
+    resPositionX = (maxX + baseX - PLACEMENTTREE_NODE_WIDTH) / 2;
+  } else {
+    resPositionX = baseX;
+    maxX = resPositionX + PLACEMENTTREE_NODE_WIDTH;
+  }
   const res = {
     id: node.id,
     data: { label: <StandardNode {...node} /> },
@@ -125,32 +141,91 @@ function buildTree(node: any, baseX: number, depth: number, tree: any[]) {
   return res;
 }
 
-function PlacementListView() {
-  const { user: me } = useAuthContext();
+function getMemberIdsWithDepth(node: any, depth: number, targetDepth: number) {
+  if (depth === targetDepth) {
+    if (node.children.length) return [{ id: node.id, value: 1 }];
+    return [{ id: node.id, value: 3 }];
+  }
+  const res: any[] = [];
+  node.children.forEach((child: any) => {
+    res.push(...getMemberIdsWithDepth(child, depth + 1, targetDepth));
+  });
 
-  const { fetchMembers, members, loading } = useFetchMembers();
+  return res.length === 0 ? [{ id: node.id, value: 3 }] : [...res, { id: node.id, value: 2 }];
+}
+
+function getResetVisibleMap(members: undefined | null | any[], me: any): Record<string, number> {
+  if (!members || members.length === 0) {
+    return {};
+  }
+
+  const { result: placementTree } = buildSponsorTree(members, me);
+  const maps = getMemberIdsWithDepth(placementTree, 0, 3);
+  const newVisibleMap: Record<string, number> = {};
+
+  maps.forEach((mp: any) => {
+    newVisibleMap[mp.id] = mp.value;
+  });
+
+  return newVisibleMap;
+}
+
+function getNewVisibleMap(
+  members: undefined | null | any[],
+  me: any,
+  visibleMap: Record<string, number>
+): Record<string, number> {
+  if (!members || !members.length) return {};
+
+  const { memberMap } = buildSponsorTree(members, me);
+  const newVisibleMap: Record<string, number> = {};
+  Object.entries(visibleMap).forEach(([id]) => {
+    if (memberMap[id]) {
+      if (memberMap[id].children.length === 0) {
+        newVisibleMap[id] = 3;
+      } else {
+        let value = 1;
+        memberMap[id].children.forEach((child: any) => {
+          if (visibleMap[child.id]) {
+            value = 2;
+          }
+        });
+        newVisibleMap[id] = visibleMap[id] === 3 ? value : visibleMap[id];
+      }
+    }
+  });
+
+  return newVisibleMap;
+}
+
+function PlacementListView() {
+  const popover = usePopover();
+
+  const { user: me } = useAuthContext();
+  const { fetchMembers, members, loading, called } = useFetchMembers();
+
+  const [visibleMap, setVisibleMap] = useState<Record<string, number>>({});
+  const exSetVisibleMap = useCallback((newVisibleMap: Record<string, number>) => {
+    setVisibleMap(newVisibleMap);
+    localStorage.setItem('sponsorVisibleMap', JSON.stringify(newVisibleMap));
+  }, []);
 
   useEffect(() => {
-    fetchMembers({
-      variables: {
-        filter: {
-          OR: [{ id: me?.id }, { sponsorId: me?.id }],
-        },
-      },
-    });
+    fetchMembers({});
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [me]);
+  }, []);
 
   const nodes: Node[] = useMemo(() => {
     if (!members || members.length === 0) return [];
-    const sponsorTree = buildSponsorTree(members, me);
+    const { result: placementTree } = buildSponsorTree(members, me);
 
     const resultTree: any[] = [];
 
-    buildTree(sponsorTree, 0, 0, resultTree);
+    buildTree(placementTree, 0, 0, resultTree, visibleMap);
 
     return resultTree;
-  }, [me, members]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [members, visibleMap]);
 
   const edges: Edge[] = useMemo(
     () =>
@@ -165,14 +240,128 @@ function PlacementListView() {
     [members]
   );
 
+  const expandTree = useCallback(
+    async (id: string) => {
+      const newVisibleMap: Record<string, number> = { ...visibleMap };
+
+      members
+        .filter((mb) => mb?.sponsorId === id)
+        .forEach((mb) => {
+          if (!newVisibleMap[mb?.id ?? '']) {
+            newVisibleMap[mb?.id ?? ''] =
+              members.findIndex((mber) => mber?.sponsorId === mb?.id) === -1 ? 3 : 1;
+          }
+        });
+
+      newVisibleMap[id] = members.findIndex((mb) => mb?.sponsorId === id) === -1 ? 3 : 2;
+
+      exSetVisibleMap(newVisibleMap);
+    },
+    [members, visibleMap, exSetVisibleMap]
+  );
+
+  const collapseTree = useCallback(
+    async (id: string) => {
+      const newVisibleMap: Record<string, number> = { ...visibleMap };
+
+      newVisibleMap[id] = members.findIndex((mb) => mb?.sponsorId === id) === -1 ? 3 : 1;
+
+      exSetVisibleMap(newVisibleMap);
+    },
+    [members, visibleMap, exSetVisibleMap]
+  );
+
+  const contextValue = useMemo(
+    () => ({
+      visibleMap,
+      expandTree,
+      collapseTree,
+    }),
+    [visibleMap, expandTree, collapseTree]
+  );
+
+  const { fitView } = useReactFlow();
+
+  const resetVisibleMap = useCallback(() => {
+    const newVisibleMap = getResetVisibleMap(members, me);
+    exSetVisibleMap(newVisibleMap);
+
+    setTimeout(() => {
+      fitView({
+        ...fitViewOptions,
+        nodes: Object.keys(newVisibleMap).map((id) => ({ id })),
+      });
+    }, 100);
+  }, [members, fitView, exSetVisibleMap, me]);
+
+  const reSyncVisibleMap = useCallback(() => {
+    const storageVisibleMap = localStorage.getItem('sponsorVisibleMap');
+    const newVisibleMap = storageVisibleMap
+      ? getNewVisibleMap(members, me, JSON.parse(storageVisibleMap))
+      : {};
+    exSetVisibleMap(newVisibleMap);
+    setTimeout(() => {
+      fitView({
+        ...fitViewOptions,
+        nodes: Object.keys(newVisibleMap).map((id) => ({ id })),
+      });
+    }, 100);
+  }, [members, exSetVisibleMap, fitView, me]);
+
+  useEffect(() => {
+    if (!called || loading) return;
+    const storageVisibleMap = localStorage.getItem('sponsorVisibleMap');
+
+    if (!storageVisibleMap || _.isEmpty(JSON.parse(storageVisibleMap))) resetVisibleMap();
+    else reSyncVisibleMap();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [members, loading]);
+
+  const reset = useCallback(async () => {
+    const { data } = await fetchMembers();
+    const newVisibleMap = getResetVisibleMap(data?.members.members, me);
+
+    exSetVisibleMap(newVisibleMap);
+
+    setTimeout(() => {
+      fitView({
+        ...fitViewOptions,
+        nodes: Object.keys(newVisibleMap).map((id) => ({ id })),
+      });
+    }, 100);
+  }, [fetchMembers, exSetVisibleMap, fitView, me]);
+
+  const refresh = useCallback(async () => {
+    const { data } = await fetchMembers();
+    const storageVisibleMap = localStorage.getItem('sponsorVisibleMap');
+    const newVisibleMap = storageVisibleMap
+      ? getNewVisibleMap(data?.members.members, me, JSON.parse(storageVisibleMap))
+      : {};
+    exSetVisibleMap(newVisibleMap);
+
+    setTimeout(() => {
+      fitView({
+        ...fitViewOptions,
+        nodes: Object.keys(newVisibleMap).map((id) => ({ id })),
+      });
+    }, 100);
+  }, [fetchMembers, exSetVisibleMap, fitView, me]);
+
   return (
     <DashboardContent sx={{ overflowX: 'hidden' }}>
       <Breadcrumbs
-        heading="Sponsor"
-        links={[{ name: 'Sponsor', href: paths.dashboard.placement.root }, { name: 'List' }]}
+        heading="Placement"
+        links={[{ name: 'Placement', href: paths.dashboard.placement.root }, { name: 'List' }]}
         sx={{
           mb: { xs: 1, md: 2 },
         }}
+        action={
+          <Stack direction="row" columnGap={1}>
+            <IconButton color={popover.open ? 'inherit' : 'default'} onClick={popover.onOpen}>
+              <Iconify icon="eva:more-horizontal-fill" />
+            </IconButton>
+          </Stack>
+        }
       />
 
       {loading ? (
@@ -180,16 +369,44 @@ function PlacementListView() {
       ) : (
         <ComponentBlock sx={{ px: 0, pb: 0 }}>
           <Stack sx={{ overflow: 'auto', height: '600px', width: '100%' }}>
-            <ReactFlow
-              nodes={nodes}
-              edges={edges}
-              fitView
-              fitViewOptions={fitViewOptions}
-              edgeTypes={edgeTypes}
-            />
+            <NodeContext.Provider value={contextValue}>
+              <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                fitView
+                fitViewOptions={fitViewOptions}
+                edgeTypes={edgeTypes}
+              />
+            </NodeContext.Provider>
           </Stack>
         </ComponentBlock>
       )}
+
+      <CustomPopover
+        open={popover.open}
+        anchorEl={popover.anchorEl}
+        onClose={popover.onClose}
+        slotProps={{ arrow: { placement: 'right-top' } }}
+      >
+        <MenuList>
+          <MenuItem
+            onClick={() => {
+              reset();
+              popover.onClose();
+            }}
+          >
+            Reset
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              refresh();
+              popover.onClose();
+            }}
+          >
+            Refresh
+          </MenuItem>
+        </MenuList>
+      </CustomPopover>
     </DashboardContent>
   );
 }
--- src/sections/Sponsor/node.tsx
+++ src/sections/Sponsor/node.tsx
@@ -1,13 +1,21 @@
+import { useContext } from 'react';
+
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import { fDate } from 'src/utils/format-time';
 
+import { Iconify } from 'src/components/Iconify';
+
+import NodeContext from './nodeContext';
+
 import type { NodeProps } from './type';
 
-export function StandardNode({ username, fullName, createdAt }: NodeProps) {
+export function StandardNode({ id, username, fullName, createdAt }: NodeProps) {
   const [firstName, lastName] = fullName ? fullName.split(' ') : ['', ''];
+
+  const { visibleMap, expandTree, collapseTree } = useContext(NodeContext);
 
   return (
     <Card
@@ -27,7 +35,6 @@ export function StandardNode({ username, fullName, createdAt }: NodeProps) {
         sx={{
           mb: 0.5,
           cursor: 'pointer',
-          '&:hover': { color: (theme) => theme.vars.palette.Alert.errorIconColor },
         }}
       >
         {`${firstName} ${lastName.length && lastName[0].toUpperCase()}.`}
@@ -46,6 +53,21 @@ export function StandardNode({ username, fullName, createdAt }: NodeProps) {
         >
           {fDate(createdAt)}
         </Typography>
+
+        <Stack direction="row" columnGap={1}>
+          <Stack>
+            {visibleMap[id] !== 3 && (
+              <Iconify
+                icon={`mdi:${visibleMap[id] === 1 ? 'plus' : 'minus'}-circle-outline`}
+                sx={{ mt: 0.15, cursor: 'pointer' }}
+                onClick={() => {
+                  if (visibleMap[id] === 1) expandTree(id);
+                  else if (visibleMap[id] === 2) collapseTree(id);
+                }}
+              />
+            )}
+          </Stack>
+        </Stack>
       </Stack>
     </Card>
   );
