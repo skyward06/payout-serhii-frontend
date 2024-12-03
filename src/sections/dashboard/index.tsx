dff48139c89c94579d924913a7ec37cb42304ecf
Stormer0528 <serhii1010@outlook.com>
2024-12-03 03:52:45 -0500

    Remove breadcrumb in dashboard

--- src/sections/dashboard/index.tsx
+++ src/sections/dashboard/index.tsx
@@ -1,25 +1,19 @@
-import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
-import { paths } from 'src/routes/paths';
-
-import { Breadcrumbs } from 'src/components/Breadcrumbs';
+import { DashboardContent } from 'src/layouts/dashboard';
 
 import Chart from '../Statistics/Chart';
 import Reward from '../Statistics/Reward';
 
 export default function Dashboard() {
   return (
-    <Container maxWidth="xl">
-      <Breadcrumbs
-        heading="Dashboard"
-        links={[{ name: 'Dashboard', href: paths.dashboard.history.root }]}
-        sx={{
-          mb: { xs: 1, md: 2 },
-        }}
-      />
+    <DashboardContent>
+      <Typography variant="h4" sx={{ pb: 2 }}>
+        Dashboard
+      </Typography>
 
       <Chart />
       <Reward />
-    </Container>
+    </DashboardContent>
   );
 }
