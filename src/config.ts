05aa1c7cb30c3fe3de0d59a8d11e021382b40e53
Stormer0528 <serhii1010@outlook.com>
2025-01-13 19:08:48 -0500

    Add SITE_URL const

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,7 @@ export type ConfigValue = {
   };
   WS_PATH: string;
   SITE_PATH: string;
+  SITE_URL: string;
   SERVER_BASE: string;
   SERVER_URL: string;
   ASSET_URL: string;
@@ -29,6 +30,7 @@ export const CONFIG: ConfigValue = {
   },
   WS_PATH: import.meta.env.VITE_WS_PATH ?? '',
   SITE_PATH: import.meta.env.VITE_SITE_PATH ?? '',
+  SITE_URL: import.meta.env.VITE_BASE_URL ?? '',
   SERVER_BASE: import.meta.env.VITE_BASE_URL ?? '',
   SERVER_URL: import.meta.env.VITE_SERVER_URL ?? '',
   ASSET_URL: import.meta.env.VITE_ASSET_URL ?? '',
