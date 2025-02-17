1098fffd8e68c33f3c479bfb06904aba4bbacf1c
Stormer0528 <serhii1010@outlook.com>
2025-02-17 12:55:44 -0500

    Update reward table in profile

--- src/sections/Profile/History/MemberStatisticsTableRow.tsx
+++ src/sections/Profile/History/MemberStatisticsTableRow.tsx
@@ -3,7 +3,7 @@ import type { MemberStatistics } from 'src/__generated__/graphql';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 
-import { fDate } from 'src/utils/format-time';
+import { formatDate } from 'src/utils/format-time';
 
 // ----------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ export default function MemberStatisticsTableRow({ row, selected }: Props) {
   const { issuedAt, member, hashPower, txcShared, percent } = row;
   return (
     <TableRow hover selected={selected}>
-      <TableCell>{fDate(issuedAt)}</TableCell>
+      <TableCell>{formatDate(issuedAt)}</TableCell>
       <TableCell>{member?.username}</TableCell>
       <TableCell>{hashPower}</TableCell>
       <TableCell>{txcShared / 10 ** 8}</TableCell>
