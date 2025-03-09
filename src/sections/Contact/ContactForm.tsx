431528856e80706d0458f85a701787227cdbcd5d
Stormer0528 <serhii1010@outlook.com>
2025-03-09 17:55:54 -0400

    Update contact form

--- src/sections/Contact/ContactForm.tsx
+++ src/sections/Contact/ContactForm.tsx
@@ -1,67 +1,14 @@
-import { useMemo } from 'react';
-import styled from 'styled-components';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import LoadingButton from '@mui/lab/LoadingButton';
-
-import { toast } from 'src/components/SnackBar';
-import { Form, Field } from 'src/components/Form';
-
-import { useContactToAdmin } from './useApollo';
-import { Schema, type SchemaType } from './schema';
-
 export default function ContactForm() {
-  const defaultValues = useMemo(() => ({ message: '' }), []);
-
-  const methods = useForm<SchemaType>({
-    resolver: zodResolver(Schema),
-    defaultValues,
-  });
-
-  const { reset, handleSubmit } = methods;
-
-  const { loading, contactToAdmin } = useContactToAdmin();
-
-  const onSubmit = handleSubmit(async (newData) => {
-    try {
-      const { data } = await contactToAdmin({ variables: { data: newData } });
-
-      if (data?.contactToAdmin.result === 'success') {
-        toast.success('Thank you! Your message has been sent.');
-      } else {
-        toast.error('Unable to send your message. Please fix errors then try again.');
-      }
-
-      reset();
-    } catch (error) {
-      console.log('error => ', error);
-    }
-  });
   return (
-    <Wrapper>
-      <Form methods={methods} onSubmit={onSubmit}>
-        <Box rowGap={3} display="grid" sx={{ mb: 3 }}>
-          <Field.Text name="name" label="Name" required />
-          <Field.Text name="email" label="Email" required />
-          <Field.Text name="description" multiline rows={4} />
-        </Box>
-
-        <SubmitButon type="submit" variant="contained" fullWidth loading={loading}>
-          SUBMIT
-        </SubmitButon>
-      </Form>
-    </Wrapper>
+    <iframe
+      title="Contact Form"
+      sandbox="allow-scripts allow-popups allow-forms allow-same-origin"
+      width="100%"
+      height="660px"
+      style={{ border: 0, overflow: 'hidden', overflowX: 'auto' }}
+      src="https://forms.helpdesk.com?licenseID=1611517858&contactFormID=21a88406-72a2-4c89-83e1-28699b1cf9ae"
+    >
+      Your browser does not allow embedded content.
+    </iframe>
   );
 }
-
-const Wrapper = styled(Paper)`
-  padding: 30px 0;
-`;
-
-const SubmitButon = styled(LoadingButton)`
-  background-color: #262262;
-  padding: 12px 0;
-`;
--- src/sections/Contact/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z as zod } from 'zod';
-
-export type SchemaType = zod.infer<typeof Schema>;
-
-export const Schema = zod.object({
-  name: zod.string({ required_error: 'Name is required' }),
-  email: zod.string({ required_error: 'Email is Required' }),
-  message: zod.string(),
-});
