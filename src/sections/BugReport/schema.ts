0d2867d064ea173ddcafcefd35ed64eaf395904d
Stormer0528 <serhii1010@outlook.com>
2025-04-25 10:19:38 -0400

    Update schema

--- src/sections/BugReport/schema.ts
+++ src/sections/BugReport/schema.ts
@@ -3,6 +3,8 @@ import { z as zod } from 'zod';
 export type SchemaType = zod.infer<typeof Schema>;
 
 export const Schema = zod.object({
-  subject: zod.string({ required_error: 'Subject is required' }),
-  description: zod.string({ required_error: 'Description is required' }),
+  who: zod.string().min(1, 'Name is required'),
+  contact: zod.string().min(1, 'Contact is required'),
+  subject: zod.string().min(1, 'Subject is required'),
+  description: zod.string().min(1, 'Description is required'),
 });
