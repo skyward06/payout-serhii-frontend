9ba92ee8046d224a680d6ff0d3f53cfbd764b456
olek <olek.mob0210@outlook.com>
2025-05-13 14:45:24 -0400

    Remove hash section in sale

--- src/sections/Sales/Hash/type.ts
+++
@@ -1,9 +0,0 @@
-import type { PaymentType } from 'src/__generated__/graphql';
-
-export type PAYMENT_METHOD_TYPE = {
-  value: PaymentType;
-  label: string;
-  icon: string;
-  backgroundColor: string;
-  disable: boolean;
-};
