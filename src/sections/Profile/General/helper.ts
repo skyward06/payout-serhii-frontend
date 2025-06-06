7df07fd0db966b425c157dbe2f9bdeaab662ce8d
Stormer0528 <serhii1010@outlook.com>
2025-06-06 13:50:35 -0400

    Update profile editform

--- src/sections/Profile/General/helper.ts
+++ src/sections/Profile/General/helper.ts
@@ -4,7 +4,7 @@ export const getWallets = (memberWallets: any) => {
   if (!Array.isArray(memberWallets)) return [[], []];
 
   const txcWallets: any[] = memberWallets
-    .filter((mw) => TXC_WALLET.findIndex((TXCWALLET) => TXCWALLET.id === mw.payout.id) !== -1)
+    .filter((mw) => TXC_WALLET.findIndex((txcWallet) => txcWallet.id === mw.payout.id) !== -1)
     .map((mw) => ({
       id: mw.id,
       payoutId: mw.payout.id,
@@ -15,13 +15,14 @@ export const getWallets = (memberWallets: any) => {
     }));
 
   const otherWallets: any = memberWallets
-    .filter((mw) => OTHER_WALLET.findIndex((TXCWALLET) => TXCWALLET.id === mw.payout.id) !== -1)
+    .filter((mw) => OTHER_WALLET.findIndex((otherWallet) => otherWallet.id === mw.payout.id) !== -1)
     .map((mw) => ({
       id: mw.id,
       payoutId: mw.payout.id,
       address: mw.address,
       note: mw.note,
       percent: mw.percent,
+      isDefault: mw.isDefault,
     }));
 
   return [txcWallets, otherWallets];
