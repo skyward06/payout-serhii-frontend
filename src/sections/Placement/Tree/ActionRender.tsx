dd8fd23b1e8c4c1bb4706f2eca212679f3480c4f
serhii <serhii1010@outlook.com>
2025-07-02 10:34:16 -0400

    Remove insert modal

--- src/sections/Placement/Tree/ActionRender.tsx
+++ src/sections/Placement/Tree/ActionRender.tsx
@@ -1,19 +1,17 @@
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
 
+import { paths } from 'src/routes/paths';
+import { useRouter } from 'src/routes/hooks';
+
 import { useBoolean } from 'src/hooks/useBoolean';
 
-import {
-  PlacementStatus,
-  type PlacementMember,
-  type PlacementToBottomInput,
-} from 'src/__generated__/graphql';
+import { type PlacementMember, type PlacementToBottomInput } from 'src/__generated__/graphql';
 
 import { Iconify } from 'src/components/Iconify';
 import { CustomPopover, type UsePopoverReturn } from 'src/components/custom-popover';
 
 import ExpandModal from './ExpandModal';
-import { InsertModal } from './InsertModal';
 
 interface Props {
   visible: number;
@@ -23,10 +21,10 @@ interface Props {
 }
 
 export function ActionRender({ visible, popover, data: placementMember, onExpandBottom }: Props) {
-  const isInsert = useBoolean();
+  const router = useRouter();
   const isExpand = useBoolean();
 
-  const { id, placementStatus } = placementMember;
+  const { id } = placementMember;
 
   return (
     <>
@@ -46,21 +44,21 @@ export function ActionRender({ visible, popover, data: placementMember, onExpand
             <Iconify icon="fluent:arrow-expand-all-16-filled" />
             Expand Bottom
           </MenuItem>
-          {placementStatus !== PlacementStatus.Temp && visible === 3 && (
+          {visible === 3 && (
             <MenuItem
-              onClick={() => {
-                isInsert.onTrue();
-                popover.onClose();
-              }}
+              onClick={() =>
+                router.push(`${paths.dashboard.sponsor.root}/new`, {
+                  state: { placementParentId: id },
+                })
+              }
             >
               <Iconify icon="mdi:plus-circle-outline" />
-              Add child
+              Add miner
             </MenuItem>
           )}
         </MenuList>
       </CustomPopover>
 
-      <InsertModal id={id} open={isInsert} />
       <ExpandModal id={id} open={isExpand} onExpandBottom={onExpandBottom} />
     </>
   );
--- src/sections/Placement/Tree/InsertModal/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z as zod } from 'zod';
-
-import { PlacementPosition } from 'src/__generated__/graphql';
-
-export type SchemaType = zod.infer<typeof Schema>;
-
-export const Schema = zod.object({
-  position: zod.enum([PlacementPosition.Left, PlacementPosition.Right, PlacementPosition.None]),
-});
