5c5d8fafb6fa2c1ab3f24a289cf2297aab8851ec
serhii <serhii1010@outlook.com>
2025-07-02 09:11:41 -0400

    Update use-router

--- src/routes/hooks/use-router.ts
+++ src/routes/hooks/use-router.ts
@@ -1,20 +1,41 @@
-import { useMemo } from 'react';
-import { useNavigate } from 'react-router-dom';
+import type { NavigateOptions } from 'react-router';
+
+import { useNavigate } from 'react-router';
+import { useMemo, useCallback } from 'react';
 
 // ----------------------------------------------------------------------
+
+/**
+ * Customized useRouter hook with NProgress integration.
+ */
 
 export function useRouter() {
   const navigate = useNavigate();
 
+  const push = useCallback(
+    (href: string, options?: NavigateOptions) => {
+      navigate(href, options);
+    },
+    [navigate]
+  );
+
+  const replace = useCallback(
+    (href: string, options?: NavigateOptions) => {
+      navigate(href, { ...options, replace: true });
+    },
+    [navigate]
+  );
+
   const router = useMemo(
     () => ({
+      push,
+      replace,
       back: () => navigate(-1),
       forward: () => navigate(1),
       refresh: () => navigate(0),
-      push: (href: string) => navigate(href),
-      replace: (href: string) => navigate(href, { replace: true }),
+      ...navigate,
     }),
-    [navigate]
+    [navigate, push, replace]
   );
 
   return router;
