61a9088bf69f2ff3da88089f567be62fa12f5235
serhii <serhii1010@outlook.com>
2025-07-24 22:04:09 -0400

    Update chart component

--- src/components/chart/chart.tsx
+++ src/components/chart/chart.tsx
@@ -1,22 +1,32 @@
 import type { BoxProps } from '@mui/material/Box';
 
-import ApexChart from 'react-apexcharts';
+import { lazy, Suspense } from 'react';
 
 import Box from '@mui/material/Box';
+
+import { ChartLoading } from './chart-loading';
 
 import type { ChartProps } from './types';
 
 // ----------------------------------------------------------------------
+
+const LazyChart = lazy(() =>
+  import('react-apexcharts').then((module) => ({ default: module.default }))
+);
 
 export function Chart({
   sx,
   type,
   series,
   height,
+  loading,
+  slotProps,
   options,
   width = '100%',
   ...other
 }: BoxProps & ChartProps) {
+  const renderFallback = () => <ChartLoading type={type} sx={slotProps?.loading} />;
+
   return (
     <Box
       dir="ltr"
@@ -30,7 +40,13 @@ export function Chart({
       }}
       {...other}
     >
-      <ApexChart type={type} series={series} options={options} width="100%" height="100%" />
+      <Suspense fallback={renderFallback()}>
+        {loading ? (
+          renderFallback()
+        ) : (
+          <LazyChart type={type} series={series} options={options} width="100%" height="100%" />
+        )}
+      </Suspense>
     </Box>
   );
 }
--- src/components/chart/types.ts
+++ src/components/chart/types.ts
@@ -3,11 +3,14 @@ import type { Theme, SxProps } from '@mui/material/styles';
 
 // ----------------------------------------------------------------------
 
-export type ChartProps = {
-  type: Props['type'];
-  series: Props['series'];
-  options: Props['options'];
-};
+export type ChartProps = React.ComponentProps<'div'> &
+  Pick<Props, 'type' | 'series' | 'options'> & {
+    sx?: SxProps<Theme>;
+    slotProps?: {
+      loading?: SxProps<Theme>;
+    };
+    loading?: boolean;
+  };
 
 export type ChartBaseProps = Props;
 
