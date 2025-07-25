a75f3ba264f92e4c266e8bd86e417f958785f8f8
serhii <serhii1010@outlook.com>
2025-07-25 10:11:15 -0400

    Move reward breadcrumb to page

--- src/pages/Reward/List.tsx
+++ src/pages/Reward/List.tsx
@@ -1,17 +1,27 @@
 import { Helmet } from 'react-helmet-async';
 
+import { paths } from 'src/routes/paths';
+
 import { CONFIG } from 'src/config';
 
-import RewardList from 'src/sections/Reward/List';
+import { Breadcrumbs } from 'src/components/Breadcrumbs';
 
-// ----------------------------------------------------------------------
+import RewardList from 'src/sections/Reward/List';
 
 export default function Page() {
   return (
     <>
       <Helmet>
-        <title>{`${CONFIG.site.name} / Reward`}</title>
+        <title>{`${CONFIG.site.name} - Reward`}</title>
       </Helmet>
+
+      <Breadcrumbs
+        heading="Reward"
+        links={[{ name: 'Reward', href: paths.dashboard.reward.root }, { name: 'List' }]}
+        sx={{
+          mb: { xs: 1, md: 2 },
+        }}
+      />
 
       <RewardList />
     </>
--- src/sections/Reward/List/index.tsx
+++ src/sections/Reward/List/index.tsx
@@ -4,7 +4,6 @@ import Tabs from '@mui/material/Tabs';
 import { useTabs } from 'src/hooks/use-tabs';
 
 import { Iconify } from 'src/components/Iconify';
-import { Breadcrumbs } from 'src/components/Breadcrumbs';
 
 import Wallets from './Wallets';
 import StatisticsTable from './Daily';
@@ -23,14 +22,6 @@ export default function RewardListView() {
 
   return (
     <>
-      <Breadcrumbs
-        heading="Reward"
-        links={[{ name: 'Reward', href: '#' }, { name: 'List' }]}
-        sx={{
-          mb: { xs: 1, md: 2 },
-        }}
-      />
-
       <Tabs value={tabs.value} onChange={tabs.onChange} sx={{ mb: { xs: 2, md: 3 } }}>
         {TABS.map((tab) => (
           <Tab key={tab.value} label={tab.label} icon={tab.icon} value={tab.value} />
