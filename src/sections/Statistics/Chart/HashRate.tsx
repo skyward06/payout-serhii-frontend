2dd25973179957a99b102fc71b0bd6d7c9bdf8be
serhii <serhii1010@outlook.com>
2025-07-25 11:01:04 -0400

    Update hashrate chart component

--- src/sections/Statistics/Chart/HashRate.tsx
+++ src/sections/Statistics/Chart/HashRate.tsx
@@ -9,6 +9,7 @@ import Card from '@mui/material/Card';
 import { formatWeekNumber } from 'src/utils/format-time';
 
 import { Chart, ChartSelect } from 'src/components/chart';
+import { useSettingsContext } from 'src/components/settings';
 
 import { useFetchBlocks } from '../useApollo';
 
@@ -22,6 +23,7 @@ const select = [
 ];
 
 export default function HashRate() {
+  const { colorScheme } = useSettingsContext();
   const [selectedSeries, setSelectedSeries] = useState('Block');
 
   const handleChangeSeries = useCallback((newValue: string) => {
@@ -96,6 +98,28 @@ export default function HashRate() {
     colors: ['#228a7c', '#ca2f31'],
     fill: {
       opacity: 0.6,
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+      custom: ({ dataPointIndex, w }) => {
+        const category = w.globals.categoryLabels.length
+          ? w.globals.categoryLabels[dataPointIndex]
+          : w.globals.labels[dataPointIndex];
+        const data = w.globals.initialSeries.map((item: any) => item.data[dataPointIndex]);
+
+        const chartData = data.reduce(
+          (
+            prev: any,
+            item: any,
+            index: number
+          ) => `${prev}<div style="display: flex; padding: 10px;"><div style="margin-right: 8px; width: 12px; height: 12px; border-radius: 50%; background-color: ${w.globals.colors[index]}; margin-top: 4px;">
+          </div><div><span style="color: ${colorScheme === 'dark' ? '#ffffff' : '#637381'}; margin-right: 5px;">${w.globals.seriesNames[index]}:</span> <span style="font-weight: bold;">${item} GH/s</span></div></div>`,
+          ''
+        );
+
+        return `<div style="background: ${colorScheme === 'dark' ? '#141A21' : '#ffffff'}; color: ${colorScheme === 'dark' ? '#ffffff' : '#6a7987'};"><div style="background: ${colorScheme === 'dark' ? '#28323D' : '#f4f6f8'}; color: ${colorScheme === 'dark' ? '#ffffff' : '#637381'}; font-weight: bold; padding: 5px 10px;">${category}</div>${chartData}</div>`;
+      },
     },
     legend: { show: false },
   };
