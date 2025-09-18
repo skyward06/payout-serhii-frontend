aabc9ef5c9031850c94cba5694d4ca0670e1c4da
serhii <serhii1010@outlook.com>
2025-09-18 10:44:21 -0400

    Update reimbursement schema

--- src/sections/Reimbursement/schema.ts
+++ src/sections/Reimbursement/schema.ts
@@ -3,9 +3,7 @@ import { z as zod } from 'zod';
 export type SchemaType = zod.infer<typeof Schema>;
 
 export const Schema = zod.object({
-  payToAddress: zod
-    .string({ required_error: 'Pay to address is required' })
-    .min(1, 'Pay to address is required'),
+  payToAddress: zod.string({ required_error: 'Pay to address is required' }),
   description: zod.string().optional(),
   requestedAmountInCent: zod.number({ required_error: 'Amount is required' }).min(1),
 });
