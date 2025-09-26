e80a80bc238e0f5815b795dddae41767d1b66da7
serhii <serhii1010@outlook.com>
2025-09-25 22:49:01 -0400

    Updte profile section

--- src/sections/Profile/General/index.tsx
+++ src/sections/Profile/General/index.tsx
@@ -192,7 +192,7 @@ export default function MemberGeneral({ me }: Props) {
     <>
       <Form methods={methods} onSubmit={onSubmit}>
         <Grid container spacing={3}>
-          <Grid md={12} xl={6}>
+          <Grid md={12} xl={6} zIndex={9999}>
             <Card sx={{ p: 3 }}>
               <Box
                 rowGap={3}
