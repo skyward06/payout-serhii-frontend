ddb7b9b1edfd8c156713ad785ec97767faceec2e
serhii <serhii1010@outlook.com>
2025-10-10 10:04:22 -0400

    Update reward chart title

--- src/sections/Profile/History/Reward.tsx
+++ src/sections/Profile/History/Reward.tsx
@@ -2,6 +2,9 @@ import { useMemo } from 'react';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
 
 import { formatDate } from 'src/utils/format-time';
 
@@ -50,9 +53,16 @@ export default function Reward() {
 
   return (
     <Card>
-      <Box typography="h6" px={3} pt={3}>
-        Reward
-      </Box>
+      <CardHeader
+        title={
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Box width={6} height={24} bgcolor="primary.main" borderRadius={1} />
+            <Typography variant="h6" fontWeight={600} color="text.primary">
+              Reward History
+            </Typography>
+          </Stack>
+        }
+      />
 
       <Box p={2}>
         <Chart type="bar" loading={loading} series={series} options={chartOptions} height={480} />
