d0b4f01d8c6f002f17fd6e65f47481f1f9f30712
serhii <serhii1010@outlook.com>
2025-11-05 20:40:25 -0500

    Update confirmAddress component

--- src/sections/ConfirmAddress/index.tsx
+++ src/sections/ConfirmAddress/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
-import { useBoolean } from 'src/hooks/useBoolean';
 
 import { toast } from 'src/components/SnackBar';
 import { Iconify } from 'src/components/Iconify';
@@ -16,9 +14,8 @@ export function ConfirmAddress() {
 
   const id = searchParams.get('token');
   const confirm = searchParams.get('confirm');
-
-  const sent = useBoolean();
-  const result = useBoolean();
+  const [sent, setSent] = useState<boolean>(false);
+  const [result, setResult] = useState<boolean>(false);
 
   const { confirmEmail } = useConfirmEmail();
 
@@ -29,38 +26,50 @@ export function ConfirmAddress() {
           const { data } = await confirmEmail({ id, correct: confirm === 'yes' });
 
           if (data?.confirmEmail5071.result === 'success') {
-            sent.onTrue();
-
-            toast.success(
-              sent.value ? 'Your commission is ready to pay' : 'Your commission was frozen'
-            );
+            setSent(true);
+            setResult(true);
+          } else {
+            setResult(false);
+            toast.error(data?.confirmEmail5071.message);
           }
         } catch (err: any) {
+          setSent(true);
           toast.error(err.message);
         }
       })();
     }
-  }, [id, confirm, sent, confirmEmail]);
+  }, [id, confirm, confirmEmail]);
 
   return (
     <Box px={4}>
-      {sent.value ? (
-        result.value ? (
-          <Box mt={3}>
-            <Box textAlign="center">
-              <Iconify icon="duo-icons:check-circle" color="primary.main" width={64} />
+      {sent ? (
+        result ? (
+          confirm === 'yes' ? (
+            <Box mt={3}>
+              <Box textAlign="center">
+                <Iconify icon="duo-icons:check-circle" color="primary.main" width={64} />
+              </Box>
+              <Typography variant="h6" textAlign="center">
+                Your commission is ready to pay.
+              </Typography>
             </Box>
-            <Typography variant="h6" textAlign="center">
-              Your address has been confirmed successfully.
-            </Typography>
-          </Box>
+          ) : (
+            <Box mt={3}>
+              <Box textAlign="center">
+                <Iconify icon="uim:times-circle" color="error.main" width={64} />
+              </Box>
+              <Typography variant="h6" textAlign="center">
+                Your commission was frozen. Please contact support team.
+              </Typography>
+            </Box>
+          )
         ) : (
           <Box mt={3}>
             <Box textAlign="center">
               <Iconify icon="uim:times-circle" color="error.main" width={64} />
             </Box>
             <Typography variant="h6" textAlign="center">
-              Your address confirmation failed. Please contact support team.
+              Your action failed.
             </Typography>
           </Box>
         )
