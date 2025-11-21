9d145941689e90184d95d399df9d92db52f49773
serhii <serhii1010@outlook.com>
2025-11-20 21:36:29 -0500

    Update signup form

--- src/sections/SignUp/index.tsx
+++ src/sections/SignUp/index.tsx
@@ -110,9 +110,9 @@ export function SignUpView({ isComponent = false }: Props) {
     () =>
       country === COUNTRY.USA
         ? payments
-        : payments.filter(
-            (payment) => payment.id !== (PAYMENT_METHOD_IDS[2] || PAYMENT_METHOD_IDS[3])
-          ),
+        : payments
+            .filter((payment) => payment.id !== PAYMENT_METHOD_IDS[2])
+            .filter((payment) => payment.id !== PAYMENT_METHOD_IDS[3]),
     [country, payments]
   );
 
