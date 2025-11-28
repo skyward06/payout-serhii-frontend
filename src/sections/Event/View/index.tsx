93866075860e043c62d7b43ead4b08b624788a8b
serhii <serhii1010@outlook.com>
2025-11-28 14:26:24 -0500

    Update calendar

--- src/sections/Event/View/index.tsx
+++ src/sections/Event/View/index.tsx
@@ -130,7 +130,7 @@ export function CalendarView() {
       <DashboardContent maxWidth="xl" sx={{ ...flexProps }}>
         {canReset && renderResults}
 
-        <Card sx={{ ...flexProps, minHeight: '70vh', my: 4 }}>
+        <Card sx={{ ...flexProps, minHeight: '72vh', my: 2 }}>
           <StyledCalendar sx={{ ...flexProps, '.fc.fc-media-screen': { flex: '1 1 auto' } }}>
             <CalendarToolbar
               date={formatDate(date, 'DD MMM YYYY')}
