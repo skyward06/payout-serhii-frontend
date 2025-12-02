57fe02e5e8087a45f3f73be2228e2830fa585c5c
serhii <serhii1010@outlook.com>
2025-12-02 11:30:27 -0500

    Update blogArticle component

--- src/sections/NewHomepage/BlogArticles.tsx
+++ src/sections/NewHomepage/BlogArticles.tsx
@@ -1,11 +1,10 @@
 import Autoplay from 'embla-carousel-autoplay';
 
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
+import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
-import { CONFIG } from 'src/config';
 import { varAlpha } from 'src/theme/styles';
 
 import { Image } from 'src/components/Image';
@@ -16,35 +15,11 @@ import {
   CarouselArrowBasicButtons,
 } from 'src/components/carousel';
 
-type ItemProp = {
-  id: string;
-  title: string;
-  subtitle: string;
-  description: string;
-};
+import { posts } from '../Post/List/const';
+
+import type { PostItemType } from '../Post/List/type';
 
 export function BlogArticles() {
-  const list: ItemProp[] = [
-    {
-      id: '1',
-      title: 'Rocketeers Closing',
-      subtitle: `Take advantage of BOGO commission before it's gone!`,
-      description: `Ending this Saturday, 22 November`,
-    },
-    {
-      id: '2',
-      title: 'Christmas Cheer',
-      subtitle: 'Join us for the Dallas Holiday Parade',
-      description: `Floats, cars, trucks & dancers & a festive parade!`,
-    },
-    {
-      id: '3',
-      title: 'Mine Update',
-      subtitle: 'Our first Fog Hashing Container comes online',
-      description: 'Major progress for the Victoria mine site',
-    },
-  ];
-
   const carousel = useCarousel({ loop: true }, [Autoplay({ playOnInit: true, delay: 8000 })]);
 
   return (
@@ -63,15 +38,15 @@ export function BlogArticles() {
       />
 
       <Carousel carousel={carousel}>
-        {list.map((item) => (
-          <CarouselItem key={item.id} item={item} />
+        {posts.map((post) => (
+          <CarouselItem key={post.slug} item={post} />
         ))}
       </Carousel>
     </Card>
   );
 }
 
-function CarouselItem({ item }: { item: ItemProp }) {
+function CarouselItem({ item }: { item: PostItemType }) {
   return (
     <Box sx={{ width: 1, height: '100%', position: 'relative' }}>
       <Box
@@ -87,22 +62,39 @@ function CarouselItem({ item }: { item: ItemProp }) {
           flexDirection: 'column',
         }}
       >
-        <Typography variant="overline" color="primary.light">
+        <Link variant="h6" color="primary.light" href={`/post/${item.slug}`}>
           {item.title}
-        </Typography>
-
-        <Link color="inherit" underline="none" variant="h5">
-          {item.subtitle}
         </Link>
 
-        <Typography variant="body2" noWrap>
+        <Typography
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+          }}
+        >
           {item.description}
+        </Typography>
+
+        <Typography
+          variant="body2"
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+          }}
+        >
+          {item.content}
         </Typography>
       </Box>
 
       <Image
         alt={item.title}
-        src={`${CONFIG.site.basePath}/assets/background/background-6.webp`}
+        src={item.coverImage}
         slotProps={{
           overlay: {
             background: (theme) =>
