45522d3b8428b7d6b498dec1806e99ea676a3651
serhii <serhii1010@outlook.com>
2026-01-09 11:09:45 -0500

    Add crypto waiting component

--- src/libs/Order/type.ts
+++ src/libs/Order/type.ts
@@ -6,20 +6,22 @@ import type {
   OrderPaymentMethod,
 } from 'src/__generated__/graphql';
 
+export type BasicOrder = {
+  __typename?: 'Order';
+  id: string;
+  ID: number;
+  expiredAt: any;
+  status: OrderStatus;
+  paidBalance: number;
+  acceptFirstTx: boolean;
+  paymentAddress?: string | null;
+  requiredBalance?: number | null;
+  paymentType?: PaymentType | null;
+  paymentToken?: PaymentToken | null;
+  paymentChain?: PaymentChain | null;
+  availablePaymentMethods: Array<OrderPaymentMethod>;
+};
+
 export type OrderContextValue = {
-  order: {
-    __typename?: 'Order';
-    id: string;
-    ID: number;
-    expiredAt: any;
-    status: OrderStatus;
-    paidBalance: number;
-    acceptFirstTx: boolean;
-    paymentAddress?: string | null;
-    requiredBalance?: number | null;
-    paymentType?: PaymentType | null;
-    paymentToken?: PaymentToken | null;
-    paymentChain?: PaymentChain | null;
-    availablePaymentMethods: Array<OrderPaymentMethod>;
-  };
+  order: BasicOrder;
 };
---
+++ src/sections/Order/CryptoWaiting.tsx
@@ -0,0 +1,196 @@
+import type { BasicOrder } from 'src/libs/Order/type';
+
+import { useMemo } from 'react';
+import QRCode from 'react-qr-code';
+
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import { useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+import { useCopyToClipboard } from 'src/hooks/use-copy-to-clipboard';
+
+import { fNumber } from 'src/utils/formatNumber';
+import { truncateMiddle } from 'src/utils/helper';
+
+import { CONFIG } from 'src/config';
+import { CHAIN_UNIT, PAYMENT_METHOD } from 'src/consts';
+
+import { toast } from 'src/components/SnackBar';
+import { Iconify } from 'src/components/Iconify';
+
+import { Timer } from './Timer';
+import { HelpView } from './Help';
+import { useCancelOrder } from './useApollo';
+
+interface Props {
+  current: BasicOrder;
+}
+
+export function CryptoWaiting({ current }: Props) {
+  const theme = useTheme();
+
+  const { copy } = useCopyToClipboard();
+
+  const ITEMS = useMemo(() => {
+    if (!current || !current?.paymentToken) return [];
+
+    const balanceUnit = PAYMENT_METHOD[current?.paymentToken]?.balance || 0;
+    const fractionDigits = CHAIN_UNIT[current?.paymentToken];
+
+    const items = [
+      {
+        label: truncateMiddle(current?.paymentAddress ?? '', 30),
+        value: current?.paymentAddress,
+        icon: 'entypo:wallet',
+        copy: true,
+      },
+    ];
+
+    if (!current.acceptFirstTx) {
+      items.push({
+        label: fNumber((current?.requiredBalance ?? 0) / balanceUnit, {
+          minimumFractionDigits: fractionDigits,
+          maximumFractionDigits: fractionDigits,
+        }),
+        value: `${(current?.requiredBalance ?? 0) / balanceUnit}`,
+        icon: `${CONFIG.site.basePath}/assets/${current?.paymentToken}.png`,
+        copy: true,
+      });
+    }
+
+    if (current?.paidBalance) {
+      items.push({
+        label: fNumber((current?.paidBalance ?? 0) / balanceUnit, {
+          minimumFractionDigits: fractionDigits,
+          maximumFractionDigits: fractionDigits,
+        }),
+        value: `${(current?.paidBalance ?? 0) / balanceUnit}`,
+        icon: `${CONFIG.site.basePath}/assets/${current?.paymentToken}.png`,
+        copy: false,
+      });
+    }
+
+    return items;
+  }, [current]);
+
+  const { cancelOrder } = useCancelOrder();
+
+  const onCopy = async (value: any) => {
+    copy(value);
+    toast.success('Copied!');
+  };
+
+  const handleCancel = async () => {
+    try {
+      await cancelOrder({ variables: { data: { id: current!.id } } });
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" mb={4}>
+        <Typography variant="h6">Detail</Typography>
+        <Box border={`2px solid ${theme.palette.success.main}`} borderRadius={0.5} p={1}>
+          <Timer />
+        </Box>
+      </Stack>
+
+      <Box>
+        <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'center', mb: 2 }}>
+          <Box sx={{ p: 2, background: '#ffffff' }}>
+            <QRCode value={current?.paymentAddress ?? ''} size={256} level="H" />
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              background: theme.palette.background.paper,
+              borderRadius: '50%',
+              padding: 0.5,
+            }}
+          >
+            <Avatar
+              src={`${CONFIG.site.basePath}/assets/${current?.paymentToken}.png`}
+              sx={{ width: 50, height: 50 }}
+            />
+          </Box>
+        </Box>
+
+        {ITEMS.map((item, index) => (
+          <Box
+            sx={{
+              mb: 1,
+              pr: 1,
+              gap: 1,
+              display: 'flex',
+              borderRadius: 0.5,
+              alignItems: 'center',
+              border: `1px solid ${theme.palette.primary.main}`,
+            }}
+          >
+            <Box
+              sx={{
+                width: 40,
+                height: 40,
+                display: 'flex',
+                alignItems: 'center',
+                background: theme.palette.primary.main,
+              }}
+            >
+              {index === 0 ? (
+                <Iconify
+                  icon={item.icon}
+                  color="#ffffff"
+                  sx={{ display: 'block', margin: 'auto' }}
+                />
+              ) : (
+                <Box
+                  sx={{
+                    background: theme.palette.background.paper,
+                    borderRadius: '50%',
+                    padding: 0.25,
+                    margin: 'auto',
+                  }}
+                >
+                  <Avatar src={item.icon} sx={{ width: 20, height: 20 }} />
+                </Box>
+              )}
+            </Box>
+
+            <Box sx={{ width: '100%' }}>
+              <Typography>{item?.label}</Typography>
+            </Box>
+
+            {item.copy ? (
+              <Iconify
+                icon="bxs:copy"
+                sx={{ cursor: 'pointer' }}
+                color={theme.palette.primary.main}
+                onClick={() => onCopy(item.value)}
+              />
+            ) : (
+              <Typography fontWeight={500}>Paid</Typography>
+            )}
+          </Box>
+        ))}
+      </Box>
+
+      <Stack direction="row" justifyContent="flex-end" mt={4}>
+        <Button variant="outlined" onClick={handleCancel}>
+          Cancel
+        </Button>
+      </Stack>
+
+      <HelpView />
+    </>
+  );
+}
